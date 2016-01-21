088cf9080f43ae4d1668d7ee62d2ac6895024cf1
Tom Lovett <tomlovett6@gmail.com>
2016-01-21 15:05:58 -0700

    updating

--- main.js
+++ main.js
@@ -7,9 +7,9 @@ angular.module('tinder')
 	$scope.displayTextarea = [false, false, false];
 
 	$scope.userData = [
-	'Lorem ipsum dolor sit amet, id alterum postulant cotidieque mea, sed nisl sadipscing intellegebat eu. Duo at deleniti delicata, at vel tibique reformidans, in vero meliore vim. Abhorreant accommodare est ut, debitis voluptaria appellantur qui ad, duo an dicit utamur.',
-	 'Vix viris officiis ei, rebum simul cetero vel ea. Et vis ludus libris tibique, quo et velit movet consequuntur. Duo brute sanctus mnesarchum at, quem definiebas eloquentiam ne qui. Etiam consectetuer cu has. Pro cu partem audiam facilis, liber epicuri scribentur vis cu.',
-	 'Tantas melius persequeris sit ne, ut facer iudicabit sea. An mei velit docendi ponderum, solum dolorum suavitate an nec, vix agam ferri iriure an. Has prodesset posidonium necessitatibus ei. Nisl aperiri virtute nam et. Errem affert sed ex, an laudem aliquando scribentur duo.']
+	'My name is Sean!',
+	 'I like Michigan State Spartans. They\'re great. They\'re my favorite. If I could play for a football team team, I would play for the Michigan State Spartans.',
+	 'I\'m great. I like going on dates if I can drink a beer on our date. I really like beer, too. I can drink my beer and you can talk and if we like eachother we can do it again!']
 
 	$scope.modifyData = function(n, event) {
 		$scope.displayTextarea[n] = true;
