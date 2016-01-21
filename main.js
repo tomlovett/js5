09772d5342966a6abe1e4dd76bfcb59a0ce5ad23
Tom Lovett <tomlovett6@gmail.com>
2016-01-20 17:32:42 -0700

    textarea covers text

--- main.js
+++ main.js
@@ -1,1 +1,17 @@
 // controller.edit = function() {}
+angular.module('tinder', []);
+
+angular.module('tinder')
+	.controller('profile', ['$scope', function($scope) {
+
+	$scope.showInput = [false, false, false];
+
+	$scope.modify = function(n) {
+		$scope.showInput[n] = true;
+	}
+
+	$scope.doneTalking = function(n) {
+		$scope.showInput[n] = false;
+	}
+
+}])
